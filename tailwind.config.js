2a1e912072ab5ab2cd98a7da0e36b93a6664193a
Einenlum <contact@einenlum.com>
2024-03-15 19:13:16 +0100

    Use local tailwind instead of CDN

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./templates/**/*.html",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
+
